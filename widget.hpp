aa7cf759da4a518fad00c5b0316d40afa41ed048
bencejuhaasz <32618702+bencejuhaasz@users.noreply.github.com>
2021-05-03 17:59:38 +0200

    The header file of the widget class

---
+++ widget.hpp
@@ -0,0 +1,16 @@
+#ifndef WIDGETS_HPP_INCLUDED
+#define WIDGETS_HPP_INCLUDED
+
+#include "graphics.hpp"
+
+class widget {
+protected:
+    int x_, y_, size_x_, size_y_;
+public:
+    widget(int x, int y, int sx, int sy);
+    virtual bool isover(int mx, int my);
+    virtual void draw() = 0;
+    virtual void handle(genv::event ev) = 0;
+    virtual std::string get() = 0;
+};
+#endif // WIDGETS_HPP_INCLUDED
