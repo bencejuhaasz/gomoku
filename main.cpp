8c6811fc82118dd27cee78d4ee2e17511db7e93f
Bence Juhasz <bencejuhaasz@github.com>
2021-05-03 18:50:54 +0200

    gameboard draw test

--- main.cpp
+++ main.cpp
@@ -4,20 +4,23 @@
 #include "graphics.hpp"
 #include "num_select.hpp"
 #include "num_set.hpp"
+#include "board.hpp"
 
 using namespace std;
 using namespace genv;
 
 class GameMaster {
   std::vector<std::vector<int>> fields;
+  board GameBoard = new board(50,50,15);
 
   GameMaster() {
     gout.open(600,600);
+    GameBoard.draw();
   }
 
 };
 
 int main() {
-  /* code */
+  GameMaster gomoku = new GameMaster();
   return 0;
 }
