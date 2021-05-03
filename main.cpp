858eee71b180a64b93ee348ea670744b8a50e981
Bence Juhasz <bencejuhaasz@github.com>
2021-05-03 18:17:12 +0200

    Added base app implementation in main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include "widget.hpp"
+#include "graphics.hpp"
+#include "num_select.hpp"
+#include "num_set.hpp"
+
+using namespace std;
+using namespace genv;
+
+class GameMaster {
+
+};
+
+int main() {
+  /* code */
+  return 0;
+}
